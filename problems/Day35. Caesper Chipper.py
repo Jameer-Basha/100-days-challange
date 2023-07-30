147581efd2d3e50286e2ea59db35757cce4ca3f6
Jameer Basha sheik <123353929+Jameer-Basha@users.noreply.github.com>
2023-07-30 11:25:06 +0530

    Rename Day35. Caesar Chipper.py

---
+++ problems/Day35. Caesper Chipper.py
@@ -0,0 +1,84 @@
+'''
+
+Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+
+Function Description
+
+Complete the caesarCipher function in the editor below.
+
+caesarCipher has the following parameter(s):
+
+string s: cleartext
+int k: the alphabet rotation factor
+Returns
+
+string: the encrypted string
+Input Format
+
+The first line contains the integer, , the length of the unencrypted string.
+The second line contains the unencrypted string, .
+The third line contains , the number of letters to rotate the alphabet by.
+
+
+Sample Input
+
+11
+middle-Outz
+2
+Sample Output
+
+okffng-Qwvb
+Explanation
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+m -> o
+i -> k
+d -> f
+d -> f
+l -> n
+e -> g
+-    -
+O -> Q
+u -> w
+t -> v
+z -> b
+
+'''
+
+def caesarCipher(s, k):
+    # Write your code here
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    res=""
+    for i in s:
+        if i.isalpha():
+            
+            if i.isupper():
+                asci = ord(i)-65
+                res += upperAlpha[(asci+k)%26]
+            if i.islower():
+                asci = ord(i)-97 
+                res += alpha[(asci+k)%26]
+        else:
+            res+=i
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
