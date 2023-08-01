9135650c4e14da86c9b53ec4576e6e7b141f5349
Jameer Basha sheik <123353929+Jameer-Basha@users.noreply.github.com>
2023-08-01 11:08:29 +0530

    Rename Day40. Alternate Even and Odd Series.py

---
+++ problems/Day40. Alternate Even and Odd Series.py
@@ -0,0 +1,41 @@
+'''
+Write a program for printing Alternate even odd series.
+
+input:- A5w8@k7!123n69
+
+output:-85276139
+
+If number of special character in the given string is even so we should print first evendigits and next odd digits alternate in the same series as they present in the string
+
+input:-#bn7856!@kn2n65jbnj482375
+
+output:-7856523674582
+
+If number of special characters in the given string is odd so we should first print odd digit and then even digit alternate in the same series as they present in the string
+
+Input Format
+
+Read a string with special cahracters and numbers
+
+Constraints
+
+None
+
+Output Format
+
+display the resultant sequence
+
+Sample Input 0
+
+A5w8@k7!123n69
+Sample Output 0
+
+85276139
+Sample Input 1
+
+#bn7856!@kn2n65jbnj482375
+Sample Output 1
+
+7856523674582
+
+'''
