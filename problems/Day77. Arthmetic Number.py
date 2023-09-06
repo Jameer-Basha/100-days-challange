bda386270c92bd26d87735d1ccde336e596f2590
Jameer Basha Sheik <123353929+Jameer-Basha@users.noreply.github.com>
2023-09-06 20:57:37 +0530

    Create Day77. Arithmetic Number.py

---
+++ problems/Day77. Arthmetic Number.py
@@ -0,0 +1,44 @@
+'''
+
+
+Given three integers  'A' denoting the first term of an arithmetic sequence , 'C' denoting the common difference of an arithmetic sequence and an integer 'B'. you need to tell whether 'B' exists in the arithmetic sequence or not. Return 1 if B is present in the sequence. Otherwise, returns 0.
+
+Example 1:
+
+Input: A = 1, B = 3, C = 2
+Output: 1
+Explaination: 3 is the second term of the 
+sequence starting with 1 and having a common 
+difference 2.
+Example 2:
+
+Input: A = 1, B = 2, C = 3
+Output: 0
+Explaination: 2 is not present in the sequence.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function inSequence() which takes A, B and C and returns 1 if B is present in the sequence. Otherwise, returns 0.
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+-109 ≤ A, B, C ≤ 109  
+
+
+
+'''
+
+class Solution:
+    def inSequence(self, A, B, C):
+        # code here
+        Diff = B-A
+        if(C==0 ):
+            if(A==B):
+                return 1
+            else:
+                return 0
+        if Diff%C==0 and Diff// C>=0:
+            return 1
+        else:
+            return 0
+
